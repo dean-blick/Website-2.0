525182b7ec1516c74f19cd2b83d39c6508ffa463
dean-blick <steveworkless@gmail.com>
2025-03-20 14:05:29 -0500

    first commit

---
+++ Website-2.0/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<header class="sticky top-0 z-50 border-b-[1px] border-surface-500/20 bg-surface-50/90 dark:bg-surface-950/90 backdrop-blur w-full h-[70px] flex items-center">
+
+</header>
+{@render children()}
---
+++ Website-2.0/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn preset-filled-primary-100-900">This is a button</button>
