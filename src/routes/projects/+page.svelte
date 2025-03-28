1166e37d0e9335ed267e40c3df542fa582509b33
dean-blick <steveworkless@gmail.com>
2025-03-28 15:03:12 -0500

    projects bones

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
     import type { PageData } from './$types';
+    import { projects } from './projectInfo';
+    import Project from '../../components/Project.svelte';
 
     let { data }: { data: PageData } = $props();
+
+    interface ProjectItem {
+        Title: string,
+        Description: string,
+        Badges: Array<string>,
+        Link: string,
+        GitHub: string
+    }
 </script>
+
+
+<div class="flex flex-col items-center lg:items-stretch lg:py-10">
+    {#each projects as project}
+        <Project info={project}/>
+    {/each}
+</div>
